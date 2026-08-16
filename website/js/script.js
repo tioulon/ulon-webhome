a02fb04f576766e8877ec5dd2f687f1733de424e
Ulon <marcuspaulo.cruz.oliveira@gmail.com>
2026-08-16 10:55:54 -0300

    Add Pocketbase connection for the Changelog window

--- website/js/script.js
+++ website/js/script.js
@@ -1,30 +1,53 @@
-// APPEND TEXT TO PLACES (CHANGELOG, TODO LIST)
+
+
+import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.25.0/dist/pocketbase.es.mjs';
+
+const pb = new PocketBase('https://api.ulon.site')
+
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('changelog-container');
-    
-    // Limpa o texto inicial
-    container.textContent = '';
-    
-    let htmlGerado = '';
-    
-    // Usa diretamente a variável que criamos no outro arquivo
-    changelogData.forEach(item => {
-        htmlGerado += `
-            <div class="changelog-item">
-                <h3> -- ${item.date}</h3>
-                <changelog-ul>
-        `;
-        
-        item.changes.forEach(changes => {
-            htmlGerado += `<li>${changes}</li>`;
+
+    async function getChangelogs() {
+
+        const records = await pb.collection('uh_changelog').getFullList({
+            sort: '-created',
         });
+
+        console.log(records.length);
+
+        const container = document.getElementById('changelog-container');
         
-        htmlGerado += `
-                </changelog-ul>
-            </div>
-        `;
-    });
-    
-    // Insere tudo otimizado na tela
-    container.insertAdjacentHTML('beforeend', htmlGerado);
+        // Limpa o texto inicial
+        container.textContent = '';
+        
+        let htmlGerado = '';
+        
+
+        for (let index = 0; index < records.length; index++) {
+            const element = records[index];
+            console.log(element);
+
+            htmlGerado += `
+                <div class="changelog-item">
+                    <h3> -- ${element.date}</h3>
+                    <changelog-ul>
+            `;
+            
+            element.content.forEach(changes => {
+                htmlGerado += `<li>${changes}</li>`;
+            });
+
+            htmlGerado += `
+                    </changelog-ul>
+                </div>
+            `;
+        }
+
+        if (records.length == 0) {
+            htmlGerado = "No Changelogs Found."
+        }
+        // Insere tudo otimizado na tela
+        container.insertAdjacentHTML('beforeend', htmlGerado);
+        
+    }
+    getChangelogs();
 });
